c94961baa00609b0a03526bc837949d3fa42e15d
张江 <3284507187@qq.com>
2022-08-02 17:17:55 +0800

    init

---
+++ App.vue
@@ -0,0 +1,67 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			this.update();
+		},
+		onHide: function() {
+			console.log('App Hide')
+		},
+		methods: {
+			update() {
+				if (wx.canIUse('getUpdateManager')) {
+					const updateManager = wx.getUpdateManager()
+					updateManager.onCheckForUpdate(function(res) {
+						console.log('onCheckForUpdate====', res)
+						// 请求完新版本信息的回调
+						if (res.hasUpdate) {
+							console.log('res.hasUpdate====')
+							updateManager.onUpdateReady(function() {
+								wx.showModal({
+									title: '更新提示',
+									content: '新版本已经准备好，是否重启应用？',
+									success: function(res) {
+										console.log('success====', res)
+										// res: {errMsg: "showModal: ok", cancel: false, confirm: true}
+										if (res.confirm) {
+											// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+											updateManager.applyUpdate()
+										}
+									}
+								})
+							})
+							updateManager.onUpdateFailed(function() {
+								// 新的版本下载失败
+								wx.showModal({
+									title: '已经有新版本了哟~',
+									content: '新版本已经上线啦~，请您删除当前小程序，重新搜索打开哟~'
+								})
+							})
+						}
+					})
+				}
+			}
+		}
+	}
+</script>
+<style lang="scss">
+	/* 注意要写在第一行，同时给style标签加入lang="scss"属性 */
+	@import "uview-ui/index.scss";
+
+	page {
+		font-family: Monospaced Number, Chinese Quote, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, PingFang SC, Hiragino Sans GB, Microsoft YaHei, Helvetica Neue, Helvetica, Arial, sans-serif !important;
+	}
+
+	view {
+		overflow: initial;
+	}
+
+	.mescroll-empty .empty-icon {
+		width: 100% !important;
+	}
+</style>
+<style>
+	/*每个页面公共css */
+</style>
---
+++ main.js
@@ -0,0 +1,54 @@
+import App from './App'
+import Vue from 'vue'
+import store from '@/store';
+Vue.config.productionTip = false
+App.mpType = 'app';
+
+// 引入全局uView
+import uView from 'uview-ui';
+Vue.use(uView);
+import '@/static/common/css/index.scss'
+
+
+import PageContainer from "@/components/PageContainer/PageContainer.vue"
+Vue.component('page-container', PageContainer)
+
+
+// i18n部分的配置
+// 引入语言包，注意路径
+import Chinese from '@/common/locales/zh.js';
+import English from '@/common/locales/en.js';
+// VueI18n
+import VueI18n from '@/common/vue-i18n.min.js';
+// VueI18n
+Vue.use(VueI18n);
+const i18n = new VueI18n({
+	// 默认语言
+	locale: 'zh',
+	// 引入语言文件
+	messages: {
+		'zh': Chinese,
+		'en': English,
+	}
+});
+
+// 由于微信小程序的运行机制问题，需声明如下一行，H5和APP非必填
+Vue.prototype._i18n = i18n;
+
+Vue.prototype.$setvuex = (name, value) => {
+	store.commit('$uStore', {
+		name,
+		value
+	})
+}
+export const app = new Vue({
+	i18n,
+	store,
+	...App
+})
+
+
+// http拦截器， 将此部分放在new Vue() 和app.$mount() 之间， 才能App.vue中正常使用
+require('@/common/http.interceptor.js')(app);
+
+app.$mount()
